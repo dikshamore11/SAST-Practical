c58d40309eaeac82dc6a529c69cabb6382a46096
dikshamore11 <dikshamore9699@gmail.com>
2026-08-06 16:46:47 +0530

    Add vulnerable Java code

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Hello {
+
+    public static void main(String[] args) throws Exception {
+
+        String userInput = "ls";
+
+        Runtime.getRuntime().exec(userInput);
+
+    }
+
+}
